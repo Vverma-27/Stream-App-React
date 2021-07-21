37a2851f94579931419f12a0042c74dcb1682b71
Vihaan Verma <vverma270705@gmail.com>
2021-07-21 23:25:22 +0530

    Redux-form integration

--- client/src/components/stream/StreamCreate.js
+++ client/src/components/stream/StreamCreate.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+const StreamCreate = (props) => {
+  const renderInput = ({ input, label, meta }) => {
+    // console.log(meta);
+    return (
+      <section
+        className={`field ${
+          !meta.active && meta.error && meta.touched ? "error" : ""
+        }`}
+      >
+        <label>{label}</label>
+        <input type="text" {...input} />
+        {(() =>
+          !meta.active && meta.error && meta.touched ? (
+            <section className="ui error message">
+              <section className="header">{meta.error}</section>
+            </section>
+          ) : null)()}
+      </section>
+    );
+  };
+  const onSubmit = (formValues) => console.log(formValues);
+  return (
+    <form onSubmit={props.handleSubmit(onSubmit)} className="ui form error">
+      <Field name="title" component={renderInput} label="Enter Title" />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter Description"
+      />
+      <button className="ui primary button">Submit</button>
+    </form>
+  );
 };
 
-export default StreamCreate;
+const validate = ({ title, description }) => {
+  const errors = {
+    title: !title && "Enter Title",
+    description: !description && "Enter Description",
+  };
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamCreate",
+  validate,
+})(StreamCreate);
